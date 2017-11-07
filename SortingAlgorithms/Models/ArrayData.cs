8965a33b85fee674cf1fd23ca659791a1e39ff91
Guest User <Guest@epicodus-8.local>
2017-11-07 15:01:02 -0800

    Add array data model for initializing a 100 element array (1 - 100) and randomize it using Fisher - Yates shuffle algorithm.

--- SortingAlgorithms/Models/ArrayData.cs
+++ SortingAlgorithms/Models/ArrayData.cs
@@ -3,14 +3,38 @@ using System.Linq;
 
 namespace SortingAlgorithms.Models
 {
-  public class ArrayData
-  {
-    private int[] _shuffledArray = new int[100];
-
-    public ArrayData()
+    public class ArrayData
     {
-      _shuffledArray = Enumerable.Range(1, 100).ToArray();
-      Console.WriteLine(_shuffledArray);
+        private int[] _arrayData;
+
+
+        public ArrayData()
+        {
+            _arrayData = Enumerable.Range(1, 100).ToArray();
+            this.ShuffleArray();
+
+            Console.WriteLine(_arrayData);
+        }
+
+        // Using the Fisher - Yates shuffle algorithm
+        // https://bost.ocks.org/mike/shuffle/
+        public void ShuffleArray()
+        {
+            int m, i, temp;
+            m = i = temp = _arrayData.Length;
+            Random _random = new Random();
+
+            while (m > 0)
+            {
+                // Finding a random element in the sorted portion of the array
+                i = (int)Math.Floor(_random.NextDouble() * m--);
+                // Assigning the value of the random index to a temporary value
+                temp = _arrayData[m];
+                // Swap the found value to the end of the array
+                _arrayData[m] = _arrayData[i];
+                // Swap the last sorted value in the array to the randomly selected index
+                _arrayData[i] = temp;
+            }
+        }
     }
-  }
 }
